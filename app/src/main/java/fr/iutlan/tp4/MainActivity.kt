dcc0c28136dc2cc9801881248e35d9babc5781c9
rabihrajaa <rajaarabihh@gmail.com>
2025-03-16 14:07:08 +0000

    commit2

--- app/src/main/java/fr/iutlan/tp4/MainActivity.kt
+++ app/src/main/java/fr/iutlan/tp4/MainActivity.kt
@@ -3,45 +3,59 @@ package fr.iutlan.tp4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import fr.iutlan.tp4.ui.theme.TP4Theme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TP4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text(
+                text = "Bonjour tout le monde !",
+                fontWeight = FontWeight.Bold,
+                fontSize = 32.sp,
+                color = Color.Magenta
+            )
         }
+//        enableEdgeToEdge()
+//        setContent {
+//            TP4Theme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+
     }
 }
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    TP4Theme {
+//        Greeting("Android")
+//    }
+//}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TP4Theme {
-        Greeting("Android")
-    }
+fun Accueil(name: String) {
+    Text(text = "Bonjour $name",fontSize=20.sp)
 }
